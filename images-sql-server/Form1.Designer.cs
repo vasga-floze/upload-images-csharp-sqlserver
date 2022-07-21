664b998cee0ff1132003fc9b975c433e96c0fd95
vasga-floze <gabriela.floze@gmail.com>
2022-07-21 11:32:43 -0600

    add initial controls

--- images-sql-server/Form1.Designer.cs
+++ images-sql-server/Form1.Designer.cs
@@ -28,20 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblArchivo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.lblTittle = new System.Windows.Forms.Label();
+            this.openFileDialogBuscarImagen = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
+            // 
+            // lblArchivo
+            // 
+            this.lblArchivo.AutoSize = true;
+            this.lblArchivo.Location = new System.Drawing.Point(95, 134);
+            this.lblArchivo.Name = "lblArchivo";
+            this.lblArchivo.Size = new System.Drawing.Size(55, 16);
+            this.lblArchivo.TabIndex = 5;
+            this.lblArchivo.Text = "Archivo:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(96, 93);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(59, 16);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(203, 93);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(329, 22);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(202, 134);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.ReadOnly = true;
+            this.txtArchivo.Size = new System.Drawing.Size(329, 22);
+            this.txtArchivo.TabIndex = 6;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(537, 129);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(119, 32);
+            this.btnAbrir.TabIndex = 7;
+            this.btnAbrir.Text = "Buscar";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            // 
+            // lblTittle
+            // 
+            this.lblTittle.AutoSize = true;
+            this.lblTittle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTittle.Location = new System.Drawing.Point(289, 28);
+            this.lblTittle.Name = "lblTittle";
+            this.lblTittle.Size = new System.Drawing.Size(221, 25);
+            this.lblTittle.TabIndex = 8;
+            this.lblTittle.Text = "CARGAR IMAGENES";
+            // 
+            // openFileDialogBuscarImagen
+            // 
+            this.openFileDialogBuscarImagen.FileName = "openFileDialog1";
             // 
             // frmUploadImages
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(796, 521);
+            this.Controls.Add(this.lblTittle);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.lblArchivo);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
             this.Name = "frmUploadImages";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblArchivo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Label lblTittle;
+        private System.Windows.Forms.OpenFileDialog openFileDialogBuscarImagen;
     }
 }
 
